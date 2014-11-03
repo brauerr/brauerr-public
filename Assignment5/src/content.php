ea5b04345fb06293bc285256892a0f5650bdf7f0
brauerr <brauerr@onid.oregonstate.edu>
2014-11-02 19:45:45 -0600

    Completed multtable.php Assignment5

--- Assignment5/src/content.php
+++ Assignment5/src/content.php
@@ -1,1 +1,16 @@
 //content.php
+
+<?php
+    //page is posted a user name
+    //if user name is empty string or null, should display message
+    //"A username must be entered, click here to return to the login screen
+    //here should be link to login.php
+    //else dipslay string 'Hello [username] you have visited this page [n] times before.
+    //click here to logout', here should be button/link - destroying session and returning
+    //to login.php
+    //n should start at 0 , etc.
+    //if user tries to access directly, should redirect to login.php
+    //ex - set variable when sesion started correctly, and check if exists before loading page content
+    //else redirect to login
+    
+?>
--- Assignment5/src/multtable.php
+++ Assignment5/src/multtable.php
@@ -1,5 +1,4 @@
 <?php
-    //multtable.php
     ini_set('display-errors', '1');
     error_reporting('E_ALL');
 ?>
@@ -9,35 +8,111 @@
     <head>
         <meta charset="utf-8">
         <title>Multiplication Table</title>
+        <link rel="stylesheet" href="assgn5Style.css">
     </head>
   
     <body>
         <?php
-            /**  some documentation
-            @category
-            @package
-            @author
-            @license
-            @link
-            @URL  https://web.engr.oregonstate.edu/~brauerr/Assignment5/src/multtable.php?max-multiplicand=9&min-multiplicand=1&max-multiplier=9&min-multiplier=1
-            */
+            //Test URL with GET values:  
+            //https://web.engr.oregonstate.edu/~brauerr/Assignment5/src/multtable.php?max-multiplicand=9&min-multiplicand=1&max-multiplier=9&min-multiplier=1
         
-            $minMultiplicand = $_GET['min-multiplicand'];
-            $maxMultiplicand = $_GET['max-multiplicand'];
-            $minMultiplier = $_GET['min-muliplier'];
-            $maxMultiplier = $_GET['max-multiplier'];
-            $width = 2 + $maxMultiplier - $minMultiplier;
-            $height = 2 + $maxMultiplicand - $minMultiplicand;
+            //set boundary values from GET superglobal array
+            $minMultiplicand = (int)$_GET['min-multiplicand'];
+            $maxMultiplicand = (int)$_GET['max-multiplicand'];
+            $minMultiplier = (int)$_GET['min-multiplier'];
+            $maxMultiplier = (int)$_GET['max-multiplier'];
+            $getValuesOk = true;
             
             //check to ensure max values are larger than min values
-            //check to ensure all values are populated and are integers
-            //create multiplication table and output to screen in valid html5
+            if ($maxMultiplicand < $minMultiplicand) {
+              echo "Minimum multiplicand larger than maximum<br>";
+              $getValuesOk = false;
+            }
+            if ($maxMultiplier < $minMultiplier) {
+                echo "Minimum multiplier larger than maximum<br>";
+                $getValuesOk = false;
+            }
+            
+            //check to ensure that all values are populated
+            if ($maxMultiplier == null) {
+                echo "Missing parameter max multiplier<br>";
+                $getValuesOk = false;
+            }            
+            if ($minMultiplier == null) {
+                echo "Missing parameter min multiplier<br>";
+                $getValuesOk = false;
+            } 
+            if ($maxMultiplicand == null) {
+                echo "Missing parameter max multiplicand<br>";
+                $getValuesOk = false;
+            }
+            if ($minMultiplicand == null ) {
+                echo "Missing parameter min mulitplicand<br>";
+                $getValuesOk = false;
+            }
+            
+            //check to ensure that all values are integers
+            if (!is_int($minMultiplicand)) {
+                echo "Min multiplicand must be an integer<br>";
+                $getValuesOk = false;
+            }
+            if (!is_int($maxMultiplicand)) {
+                echo "Max multiplicand must be an integer<br>";
+                $getValuesOk = false;
+            }
+            if (!is_int($minMultiplier)) {
+                echo "Min multiplier must be an integer<br>";
+                $getValuesOk = false;
+            }
+            if (!is_int($maxMultiplier)) {
+                echo "Max multiplier must be an integer<br>";
+                $getValuesOk = false;
+            }
 
-            echo $minMultiplicand;
-            echo $maxMultiplicand;
-        
-            echo '<p>Hello World</p>';
-            echo $_SERVER['HTTP_USER_AGENT'];
+            //if all checks pass, call function printMultTable
+            if ($getValuesOk) {
+                printMultTable($minMultiplicand, $maxMultiplicand, $minMultiplier, $maxMultiplier);
+            }
+            
+            function printMultTable($minMultiplicand, $maxMultiplicand, $minMultiplier, $maxMultiplier) {
+                //set width and height of table
+                $width = 2 + $maxMultiplier - $minMultiplier;
+                $height = 2 + $maxMultiplicand - $minMultiplicand;
+                $i = 0;
+                $j = 0;
+                $value = 0;
+                
+                //create table - dynamically create a populate rows and cells within rows
+                //create array with resulting values separately?
+                echo '<table>
+                <caption>Multiplication Table</caption>
+                <tbody>
+                <tr>
+                <th>';
+                
+                //create header row
+                for ($j = 0; $j < $width - 1; $j++) {
+                  $value = $minMultiplier + $j;
+                  echo "<th>$value";
+                }
+                
+                //populate additional rows
+                for ($i = 0; $i < $height - 1; $i++) {
+                    echo '<tr>';
+                    //print multiplicand value in first column
+                    $value = $minMultiplicand + $i;
+                    echo "<td>$value";
+                    
+                    for ($j = 0; $j < $width - 1; $j++) {
+                        $value = ($minMultiplier + $j) * ($minMultiplicand + $i);
+                        echo "<td>$value";
+                    }
+                }
+                
+                //close html table tags
+                echo '</tbody>
+                </table>';
+            }
         ?>
     
     </body>
